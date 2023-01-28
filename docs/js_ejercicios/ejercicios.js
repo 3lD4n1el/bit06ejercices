9ac586fbec62e876302374858b4b87419e773066
Daniel Romero <romerohoyosd@gmail.com>
2023-01-28 11:37:16 -0500

    completando js

--- docs/js_ejercicios/ejercicios.js
+++ docs/js_ejercicios/ejercicios.js
@@ -1,2 +1,11 @@
-'use strict'
+window.onload=inicio;
+
+function inicio(){
+    //document.querySelector("boton").onclick=listar;
+}
+function listar(){
+    let nombre=document.querySelector("campo").value;
+    document.querySelector("caja").innerHTML+=`<div>${nombre}</div>`;
+
+}
 
