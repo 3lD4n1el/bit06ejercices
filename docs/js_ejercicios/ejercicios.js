b761fd54f749f5e3aef0c1cdfd069c62cf5362f1
Daniel Romero <romerohoyosd@gmail.com>
2023-01-28 11:46:42 -0500

    fix

--- docs/js_ejercicios/ejercicios.js
+++ docs/js_ejercicios/ejercicios.js
@@ -2,6 +2,7 @@
 function inicio(){
     document.getElementById("boton").onclick=listar;
 }
+
 function listar(){
     let nombre=document.getElementById("campo").value;
     document.getElementById("respusta").innerHTML+=`${nombre}</div>`;
