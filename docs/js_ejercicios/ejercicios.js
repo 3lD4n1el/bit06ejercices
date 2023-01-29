26f01e4aedbf210db9492eb5ff545fd5a64ec790
Daniel Romero <romerohoyosd@gmail.com>
2023-01-29 15:00:44 -0500

    js 1 complete

--- docs/js_ejercicios/ejercicios.js
+++ docs/js_ejercicios/ejercicios.js
@@ -1,11 +1,11 @@
-
-function inicio(){
-    document.getElementById("boton").onclick=listar;
-}
-
-function listar(){
-    let nombre=document.getElementById("campo").value;
-    document.getElementById("respusta").innerHTML+=`${nombre}</div>`;
-
-}
-
+//ejercicio N° 1
+function saludar() {
+    let nombre = document.getElementById("input").value;
+    if (nombre === null || nombre === "") {
+      alert("espacio vacio");
+    } else {
+      document.getElementById("resultado").innerHTML + nombre;{
+      alert(resultado.value = ("Hola " + nombre + " ,bienvenido a mis ejercicios" )); 
+    }
+  }}
+  //ejercicio N° 2
